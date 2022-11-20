7d60f4a7b3f335d89afff27f76de7b3ddea3839b
Wilson Chan <wilsonchan54@gmail.com>
2022-11-20 16:40:43 -0500

    Create Sidebar component and connect it to Feed component / changed hover color in index.css

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
 
 const Feed = () => {
@@ -8,7 +9,9 @@ const Feed = () => {
       <Box sx={{ height: { sx:'auto', md:'92vh'}, 
       borderRight:'2px solid yellow', 
       px:{ sx:0, md:2}}}>
-        Sidebar
+
+        <Sidebar />
+        
         <Typography className='copyright' variant='body2'
         sx={{ mt:1.5, color: '#fff' }}  >
           Copyright 2022 Tubular Vision
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { categories } from './utils/constants';
+
+const Sidebar = () => {
+  return (
+    <Stack
+    sx={{ overflowY:'auto', 
+    height:{ sx:'auto', md:'95%'},
+    flexDirection:{ md:'column' }}}
+    >
+      {categories.map((category)=> (
+        <button
+        className='category-btn'
+        >
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  )
+}
+
+export default Sidebar
