36b54908619b32450bfa9f749866a4e77d7c8ade
Wilson Chan <wilsonchan54@gmail.com>
2022-11-28 23:36:12 -0500

    Added CardContent API to display title and channel name under thumbnail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,13 +11,31 @@ const VideoCard = ({ video:{ id:{videoId}, snippet } }) => {    // destructured
   
   return (
     <Card>
+      {/* Thumbnail */}      
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
           image={snippet?.thumbnails?.high?.url} 
           alt={snippet?.title}
-          sx={{ width:350, height:180 }}  
+          sx={{ width:'350px', height:'180px' }}  
         />
       </Link>
+      
+      {/* Video Description */}
+      <CardContent sx={{ backgroundColor:'darkgreen', height:'100px' }} > 
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant='subtitle1' fontWeight='bold' color='palevioletred'>
+            {snippet?.title.slice(0,60) ||
+            demoVideoTitle.slice(0,60)}
+          </Typography>
+        </Link>
+
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' fontWeight='bold' color='yellow'>
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontsize:12, color:'lightblue', ml:'5px'}} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
