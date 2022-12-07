3ebe4129d183a5a82446631104cd77ea85fed306
Wilson Chan <wilsonchan54@gmail.com>
2022-12-07 08:59:21 -0500

    Created ChannelDetail component to fetch channel data

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box } from '@mui/material';
+import { Videos, ChannelCard } from './allComponents';
+import { useParams } from 'react-router-dom';
+import { fetchYoutubeAPI } from './utils/fetchYoutubeAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const { id } = useParams();
+
+  console.log(channelDetail)
+
+  useEffect(()=> {
+    fetchYoutubeAPI(`channels?part="snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+  }, [id])
+
+  
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
