b9a03df4f026aedde1ee49b9fb7452daa3dbe8e8
Wilson Chan <wilsonchan54@gmail.com>
2022-12-10 23:40:12 -0500

    Added useState videos to fetch video data / rendered gradient banner and channelCard component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -8,16 +8,28 @@ import { fetchYoutubeAPI } from './utils/fetchYoutubeAPI';
 const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null);
   const { id } = useParams();
+  const [videos, setVideos] = useState([]);
 
-  console.log(channelDetail)
+  console.log(channelDetail, videos)
 
   useEffect(()=> {
     fetchYoutubeAPI(`channels?part="snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+    fetchYoutubeAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items[0]))
   }, [id])
 
   
   return (
-    <div>{id}</div>
+    //<div>{id}</div>,
+    <Box minHeight="95vh">
+      <div
+        style={{
+          background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(64,102,143,1) 50%, rgba(0,212,255,1) 100%)",
+          zIndex: 10,
+          height: "300px"
+        }}
+      />
+      <ChannelCard channelDetail={channelDetail} />
+    </Box>
   )
 }
 
