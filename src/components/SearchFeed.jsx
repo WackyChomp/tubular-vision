c3100b7549407cfc2e43ade34ad334bec019c849
Wilson Chan <wilsonchan54@gmail.com>
2022-12-16 00:54:33 -0500

    Creating SearchFeed component based on Feed component (WIP)

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { Videos } from './allComponents';
+import { fetchYoutubeAPI } from './utils/fetchYoutubeAPI';
+
 
 const SearchFeed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('Dynamic');
+  const [videos, setVideos] = useState([]);
+
+  useEffect (() => {
+    fetchYoutubeAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
+
   return (
-    <div>SearchFeed</div>
+    <Box
+    p={2}j
+    sx={{ overflowY:'auto', height:'90vh', flex:2 }}
+    >
+      <Typography
+      variant='h4' fontWeight='bold' mb={2}
+      sx={{ color:'cyan' }}
+      >
+        {selectedCategory} <span style={{ color:'yellow' }}>Videos :)</span>
+      </Typography>
+
+      <Videos videos={ videos } />
+    </Box>
   )
 }
 
